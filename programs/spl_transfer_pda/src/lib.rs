0d9bc242b2836e9d76534a1e2e6bc5d8f83ffde5
depressedliza <danilaskablov@list.ru>
2024-07-14 19:15:26 +0300

    update tests, add app_wallet to rewards

--- programs/spl_transfer_pda/src/lib.rs
+++ programs/spl_transfer_pda/src/lib.rs
@@ -1,13 +1,59 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
+use anchor_spl::token::{Mint, Token, TokenAccount, Transfer, TransferChecked};
+use std::str::FromStr;
+use anchor_lang::solana_program::clock::UnixTimestamp;
 
 declare_id!("3bgJ8TjDebaWM9PRZXLFoviajmunXPGdbWwYBxVsGdme");
+
+const STAKE_AMOUNT: u64 = 72_000_000;
+// const ADMIN_KEY: Pubkey = Pubkey::from_str("Ah2XCFjHK9kPKuqB2FYYZpwTfE882kzzGdNFRZJ6Go4w").unwrap();
+const ADMIN_KEY: &str = "Ah2XCFjHK9kPKuqB2FYYZpwTfE882kzzGdNFRZJ6Go4w";
+const TOKEN_MINT: &str = "Bckayy6RpSsBxC2wZKXKeNMVAAnvJeZT23U7DW4CDdAa";
 
 #[program]
 pub mod spl_transfer_pda {
     use super::*;
 
+    pub fn init(ctx: Context<Init>) -> Result<()> {
+        let admin_key = Pubkey::from_str(ADMIN_KEY).unwrap();
+        let token_key = Pubkey::from_str(TOKEN_MINT).unwrap();
+
+        require!(ctx.accounts.mint_of_token_being_sent.key() == token_key, ErrorCodes::IncorrectTokenMint);
+        require!(ctx.accounts.admin_ata.amount >= STAKE_AMOUNT, ErrorCodes::NotEnoughTokens);
+        require!(ctx.accounts.admin.key() == admin_key, ErrorCodes::Forbidden);
+
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.admin_ata.to_account_info(),
+            to: ctx.accounts.app_wallet.to_account_info(),
+            authority: ctx.accounts.admin.to_account_info()
+        };
+
+        let bump = ctx.bumps.app_wallet;
+        let binding = [bump];
+        let seed: Vec<&[u8]> = vec![
+            b"app_wallet",
+            &binding
+        ];
+
+        let outer = vec![seed.as_slice()];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+            outer.as_slice()
+        );
+
+        anchor_spl::token::transfer(cpi_ctx, STAKE_AMOUNT)?;
+
+        Ok(())
+    }
+
     pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        require!(ctx.accounts.stake.amount == 0, ErrorCodes::DepositAlreadyMade);
+
+        let token_key = Pubkey::from_str(TOKEN_MINT).unwrap();
+        require!(ctx.accounts.mint_of_token_being_sent.key() == token_key, ErrorCodes::IncorrectTokenMint);
+
         let transfer_instruction = Transfer {
             from: ctx.accounts.wallet_to_withdraw_from.to_account_info(),
             to: ctx.accounts.tmp_wallet.to_account_info(),
@@ -15,10 +61,15 @@ pub mod spl_transfer_pda {
         };
 
         let user_sending_key = ctx.accounts.user_sending.key();
+
+        let bump = ctx.bumps.tmp_wallet;
+        let binding = [bump];
         let seed = vec![
-            b"wallet".as_ref(),
-            user_sending_key.as_ref()
+            b"wallet",
+            user_sending_key.as_ref(),
+            &binding
         ];
+
         let outer = vec![seed.as_slice()];
 
         let cpi_ctx = CpiContext::new_with_signer(
@@ -30,21 +81,34 @@ pub mod spl_transfer_pda {
         anchor_spl::token::transfer(cpi_ctx, amount)?;
 
         ctx.accounts.stake.amount = amount;
+        ctx.accounts.stake.signer_key = ctx.accounts.user_sending.key();
+        ctx.accounts.stake.start = Clock::get()?.unix_timestamp;
+
         Ok(())
     }
 
     pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
-        let transfer_instruction = Transfer {
+        let token_key = Pubkey::from_str(TOKEN_MINT).unwrap();
+        require!(ctx.accounts.mint_of_token_being_sent.key() == token_key, ErrorCodes::IncorrectTokenMint);
+        require!(ctx.accounts.stake.amount > 0, ErrorCodes::NotEnoughTokens);
+
+        let transfer_instruction = TransferChecked {
             from: ctx.accounts.tmp_wallet.to_account_info(),
             to: ctx.accounts.wallet_to_withdraw_from.to_account_info(),
-            authority: ctx.accounts.user_sending.to_account_info()
+            authority: ctx.accounts.tmp_wallet.to_account_info(),
+            mint: ctx.accounts.mint_of_token_being_sent.to_account_info()
         };
 
         let user_sending_key = ctx.accounts.user_sending.key();
+
+        let bump = ctx.bumps.tmp_wallet;
+        let binding = [bump];
         let seed = vec![
-            b"wallet".as_ref(),
-            user_sending_key.as_ref()
+            b"wallet",
+            user_sending_key.as_ref(),
+            &binding
         ];
+
         let outer = vec![seed.as_slice()];
 
         let cpi_ctx = CpiContext::new_with_signer(
@@ -53,11 +117,77 @@ pub mod spl_transfer_pda {
             outer.as_slice()
         );
 
-        anchor_spl::token::transfer(cpi_ctx, ctx.accounts.stake.amount)?;
+        anchor_spl::token::transfer_checked(
+            cpi_ctx,
+            ctx.accounts.stake.amount,
+            ctx.accounts.mint_of_token_being_sent.decimals)?;
+
+        let reward_instruction = TransferChecked {
+            from: ctx.accounts.app_wallet.to_account_info(),
+            to: ctx.accounts.wallet_to_withdraw_from.to_account_info(),
+            authority: ctx.accounts.app_wallet.to_account_info(),
+            mint: ctx.accounts.mint_of_token_being_sent.to_account_info()
+        };
+
+        let bump = ctx.bumps.app_wallet;
+        let binding = [bump];
+        let seed: Vec<&[u8]> = vec![
+            b"app_wallet",
+            &binding
+        ];
+
+        let outer = vec![seed.as_slice()];
+        let reward_cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            reward_instruction,
+            outer.as_slice()
+        );
+
+        // calculate stake value
+        let reward_value: u64 = 10;
+        if ctx.accounts.app_wallet.amount < reward_value {
+            return Ok(());
+        }
+
+        anchor_spl::token::transfer_checked(
+          reward_cpi_ctx,
+          reward_value,
+          ctx.accounts.mint_of_token_being_sent.decimals
+        )?;
+
+        ctx.accounts.stake.amount = 0;
 
         Ok(())
     }
 
+}
+
+#[derive(Accounts)]
+pub struct Init<'info> {
+    pub mint_of_token_being_sent: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    #[account(
+        init,
+        payer = admin,
+        seeds = [b"app_wallet"],
+        bump,
+        token::mint = mint_of_token_being_sent,
+        token::authority = app_wallet
+    )]
+    pub app_wallet: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        constraint = admin_ata.owner == admin.key(),
+        constraint = admin_ata.mint == mint_of_token_being_sent.key()
+    )]
+    pub admin_ata: Account<'info, TokenAccount>,
+
+    token_program: Program<'info, Token>,
+    system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -69,19 +199,29 @@ pub struct Withdraw<'info> {
 
     #[account(
         mut,
-        seeds = [b"wallet".as_ref(), user_sending.key().as_ref()],
+        seeds = [b"wallet", user_sending.key().as_ref()],
         bump,
         token::mint = mint_of_token_being_sent,
-        token::authority = user_sending
+        token::authority = tmp_wallet,
     )]
     pub tmp_wallet: Account<'info, TokenAccount>,
 
     #[account(
         mut,
-        seeds = [b"stake".as_ref(), user_sending.key().as_ref()],
-        bump
+        seeds = [b"stake", user_sending.key().as_ref()],
+        bump,
+        constraint = stake.signer_key == user_sending.key(),
     )]
     pub stake: Account<'info, Stake>,
+
+    #[account(
+        mut,
+        seeds = [b"app_wallet"],
+        bump,
+        token::mint = mint_of_token_being_sent,
+        token::authority = app_wallet
+    )]
+    pub app_wallet: Account<'info, TokenAccount>,
 
     #[account(
         mut,
@@ -98,25 +238,25 @@ pub struct Withdraw<'info> {
 pub struct Deposit<'info> {
     pub mint_of_token_being_sent: Account<'info, Mint>, // our token
 
+    #[account(
+        init_if_needed,
+        seeds = [b"wallet", user_sending.key().as_ref()],
+        bump,
+        payer = user_sending,
+        token::mint = mint_of_token_being_sent,
+        token::authority = tmp_wallet
+    )]
+    pub tmp_wallet: Account<'info, TokenAccount>,
+
     #[account(mut)]
     pub user_sending: Signer<'info>,
 
     #[account(
-        init,
+        init_if_needed,
         payer = user_sending,
-        seeds = [b"wallet".as_ref(), user_sending.key().as_ref()],
+        seeds = [b"stake", user_sending.key().as_ref()],
         bump,
-        token::mint = mint_of_token_being_sent,
-        token::authority = user_sending
-    )]
-    pub tmp_wallet: Account<'info, TokenAccount>,
-
-    #[account(
-        init,
-        payer = user_sending,
-        seeds = [b"stake".as_ref(), user_sending.key().as_ref()],
-        bump,
-        space = 8 + 8
+        space = 8 + 8 + 32 + 16
     )]
     pub stake: Account<'info, Stake>,
 
@@ -133,5 +273,16 @@ pub struct Deposit<'info> {
 
 #[account]
 pub struct Stake {
-    pub amount: u64
+    pub amount: u64,
+    pub signer_key: Pubkey,
+    pub start: i64
+}
+
+#[error_code]
+pub enum ErrorCodes {
+    NotEnoughTokens,
+    IncorrectTokenMint,
+    Forbidden,
+    IncorrectDepositValue,
+    DepositAlreadyMade
 }
--- tests/spl_transfer_pda.ts
+++ tests/spl_transfer_pda.ts
@@ -2,8 +2,11 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SplTransferPda } from "../target/types/spl_transfer_pda";
 import {createMint, getOrCreateAssociatedTokenAccount, mintToChecked, TOKEN_PROGRAM_ID} from "@solana/spl-token";
+import * as assert from "node:assert";
 
 const LAMPORT_PER_SOL = 1000000000;
+const ADMIN_SECRET = new Uint8Array([130,  48, 202,   8,  31,  74, 143, 100,  64, 114, 166, 66, 132, 155,  54, 209, 118,  90,  56, 189, 216, 176, 168, 121, 232, 114,  31,  61, 196, 124, 175, 202, 143, 250,  34,  79, 254, 211, 237, 128,  37, 151, 253, 122, 189,  75, 128,  67, 236, 175,  68, 138, 101,  19,  21, 200, 254,  44, 208,  24,  65,  70,  98, 164])
+const TOKEN_SECRET = new Uint8Array([ 78, 234, 114,  83, 170,   3,  69, 179,  91,  13, 155, 96,  13,  61,  85, 197, 108, 208,  46,   4, 153,  11, 143,  40,  85, 215, 209, 162, 139, 218,  43,  18, 157, 189, 122, 245, 240, 128, 181, 240,  92, 228, 148, 144, 243,  95,  42, 165,  62,  51, 133,  86, 176,  57,  53, 44,  39, 130, 123,  73, 170, 244, 108, 235])
 
 describe("spl_transfer_pda", () => {
   // Configure the client to use the local cluster.
@@ -15,9 +18,29 @@ describe("spl_transfer_pda", () => {
 
   let mintPubKey = null;
   let fromAta = null;
+  let adminAta = null;
+
+  const adminWallet = anchor.web3.Keypair.fromSecretKey(ADMIN_SECRET);
+  console.log(adminWallet.secretKey);
+  console.log(adminWallet.publicKey);
 
   const fromWallet = anchor.web3.Keypair.generate();
   const authority = anchor.web3.Keypair.generate();
+
+    const [vaultPda, _vaultBump] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("wallet"), fromWallet.publicKey.toBuffer()],
+        program.programId
+    );
+
+    const [stakePda, _stakeBump] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("stake"), fromWallet.publicKey.toBuffer()],
+        program.programId
+    );
+
+    const [appWallet, _] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("app_wallet")],
+        program.programId
+    );
 
   it("Is init", async () => {
     console.log("Requesting airdrop..")
@@ -31,13 +54,29 @@ describe("spl_transfer_pda", () => {
         "confirmed"
     );
 
+    await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(adminWallet.publicKey, LAMPORT_PER_SOL * 1000),
+        "confirmed"
+    );
+
     console.log("Creating token mint..")
+    const mintKeyPair = anchor.web3.Keypair.fromSecretKey(TOKEN_SECRET);
+
     mintPubKey = await createMint(
         provider.connection,
         authority,
         authority.publicKey,
         null,
-        8);
+        8,
+        mintKeyPair);
+
+    console.log("Creating ATA to 'Admin' wallet");
+    adminAta = await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        authority,
+        mintPubKey,
+        adminWallet.publicKey
+    );
 
     console.log("Creating ATA to 'From' wallet");
     fromAta = await getOrCreateAssociatedTokenAccount(
@@ -58,24 +97,34 @@ describe("spl_transfer_pda", () => {
         8
     );
 
+    console.log("Mint tokens to admin wallet");
+    await mintToChecked(
+        provider.connection,
+        adminWallet,
+        mintPubKey,
+        adminAta.address,
+        authority,
+        72_000_000e8,
+        8
+    );
+
+    console.log("Starting initialize..");
+    await program.methods
+        .init()
+        .accounts({
+          mintOfTokenBeingSent: mintPubKey,
+          admin: adminWallet.publicKey,
+          appWallet: appWallet,
+          adminAta: adminAta.address
+        })
+        .signers([adminWallet])
+        .rpc();
+
   });
 
   it("deposit", async () => {
-    const [vaultPda, _vaultBump] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("wallet"), fromWallet.publicKey.toBuffer()],
-        program.programId
-    );
-
-    const [stakePda, _] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("stake"), fromWallet.publicKey.toBuffer()],
-        program.programId
-    );
-
-    console.log("VaultPda - ", vaultPda);
-    console.log("VaultBump - ", _vaultBump);
-
     const beforeBalance = await provider.connection.getTokenAccountBalance(fromAta.address);
-    console.log("Before balance - ", beforeBalance);
+    console.log("Before balance - ", beforeBalance.value.amount);
 
     await program.methods
         .deposit(new anchor.BN(1e8))
@@ -84,63 +133,126 @@ describe("spl_transfer_pda", () => {
           userSending: fromWallet.publicKey,
           tmpWallet: vaultPda,
           walletToWithdrawFrom: fromAta.address,
-          stake: stakePda
+          stake: stakePda,
         })
         .signers([fromWallet])
         .rpc();
 
     let afterBalance = await provider.connection.getTokenAccountBalance(fromAta.address);
-    console.log("After balance - ", afterBalance);
+    console.log("After balance - ", afterBalance.value.amount);
 
-    let tmpWalletBalance = await provider.connection.getTokenAccountBalance(vaultPda);
-    console.log("Tmp wallet balance - ", tmpWalletBalance);
+    const tmpWalletBalance = await provider.connection.getTokenAccountBalance(vaultPda);
+    console.log("Tmp wallet balance - ", tmpWalletBalance.value.amount);
   });
 
+  it("duplicate deposit", async() => {
+        try {
+            await program.methods
+                .deposit(new anchor.BN(2e8))
+                .accounts({
+                    mintOfTokenBeingSent: mintPubKey,
+                    userSending: fromWallet.publicKey,
+                    tmpWallet: vaultPda,
+                    walletToWithdrawFrom: fromAta.address,
+                    stake: stakePda,
+                })
+                .signers([fromWallet])
+                .rpc();
+
+            assert.fail("Contract allowed duplicate deposit");
+        } catch {
+            assert.ok("Contract throw exception on duplicate deposit");
+        }
+  })
+
   it("withdraw", async () => {
-    const [vaultPda, _vaultBump] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("wallet"), fromWallet.publicKey.toBuffer()],
-        program.programId
-    );
-
-    const [stakePda, _] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("stake"), fromWallet.publicKey.toBuffer()],
-        program.programId
-    );
-
     const beforeBalance = await provider.connection.getTokenAccountBalance(fromAta.address);
-    console.log("Before balance - ", beforeBalance);
+    console.log("Before balance - ", beforeBalance.value.amount);
 
     await program.methods
-        .withdraw()
-        .accounts({
+      .withdraw()
+      .accounts({
           mintOfTokenBeingSent: mintPubKey,
           userSending: fromWallet.publicKey,
-          tmpWallet:vaultPda,
           walletToWithdrawFrom: fromAta.address,
-          stake: stakePda
-        })
-        .signers([fromWallet])
-        .rpc();
+          stake: stakePda,
+          tmpWallet: vaultPda,
+          appWallet: appWallet
+      })
+      .signers([fromWallet])
+      .rpc();
 
     let afterBalance = await provider.connection.getTokenAccountBalance(fromAta.address);
-    console.log("After balance - ", afterBalance);
+    console.log("After balance - ", afterBalance.value.amount);
 
     let tmpWalletBalance = await provider.connection.getTokenAccountBalance(vaultPda);
-    console.log("Tmp wallet balance - ", tmpWalletBalance);
+    console.log("After tmp wallet balance - ", tmpWalletBalance.value.amount);
+  });
 
-    // await program.methods
-    //     .deposit(new anchor.BN(1e8))
-    //     .accounts({
-    //       mintOfTokenBeingSent: mintPubKey,
-    //       userSending: fromWallet.publicKey,
-    //       tmpWallet: vaultPda,
-    //       walletToWithdrawFrom: fromAta.address,
-    //       stake: stakePda
-    //     })
-    //     .signers([fromWallet])
-    //     .rpc();
-    //
-    // tmpWalletBalance = await provider.connection.getTokenAccountBalance(vaultPda);
-    // console.log("Tmp wallet balance again - ", tmpWalletBalance);
+  it("duplicate withdraw", async () => {
+      try {
+          await program.methods
+              .withdraw()
+              .accounts({
+                  mintOfTokenBeingSent: mintPubKey,
+                  userSending: fromWallet.publicKey,
+                  walletToWithdrawFrom: fromAta.address,
+                  stake: stakePda,
+                  tmpWallet: vaultPda,
+                  appWallet: appWallet
+              })
+              .signers([fromWallet])
+              .rpc();
+          assert.fail("Contract allowed duplicate withdraw");
+      } catch {
+          assert.ok("Contract throw exception on duplicate withdraw");
+      }
+  });
+
+  it("try again deposit", async() => {
+      const beforeBalance = await provider.connection.getTokenAccountBalance(fromAta.address);
+      console.log("Before balance - ", beforeBalance.value.amount);
+
+      await program.methods
+          .deposit(new anchor.BN(1e8))
+          .accounts({
+              mintOfTokenBeingSent: mintPubKey,
+              userSending: fromWallet.publicKey,
+              tmpWallet: vaultPda,
+              walletToWithdrawFrom: fromAta.address,
+              stake: stakePda,
+          })
+          .signers([fromWallet])
+          .rpc();
+
+      let afterBalance = await provider.connection.getTokenAccountBalance(fromAta.address);
+      console.log("After balance - ", afterBalance.value.amount);
+
+      const tmpWalletBalance = await provider.connection.getTokenAccountBalance(vaultPda);
+      console.log("Tmp wallet balance - ", tmpWalletBalance.value.amount);
+  });
+
+  it("try again withdraw", async() => {
+      const beforeBalance = await provider.connection.getTokenAccountBalance(fromAta.address);
+      console.log("Before balance - ", beforeBalance.value.amount);
+
+      await program.methods
+          .withdraw()
+          .accounts({
+              mintOfTokenBeingSent: mintPubKey,
+              userSending: fromWallet.publicKey,
+              walletToWithdrawFrom: fromAta.address,
+              stake: stakePda,
+              tmpWallet: vaultPda,
+              appWallet: appWallet
+          })
+          .signers([fromWallet])
+          .rpc();
+
+      let afterBalance = await provider.connection.getTokenAccountBalance(fromAta.address);
+      console.log("After balance - ", afterBalance.value.amount);
+
+      let tmpWalletBalance = await provider.connection.getTokenAccountBalance(vaultPda);
+      console.log("After tmp wallet balance - ", tmpWalletBalance.value.amount);
   })
 });
